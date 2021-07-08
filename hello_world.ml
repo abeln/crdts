5fd9630870770def0e4af7a6e0e459737dcda56c
Abel Nieto <abeln@cs.au.dk>
2021-07-08 15:55:07 -0400

    move io_loop out of async

--- hello_world.ml
+++ hello_world.ml
@@ -1,5 +1,6 @@
 
-open Core
+
+open Base
 open Async
 
 module VC = struct
@@ -92,7 +93,7 @@ module type COUNTER = sig
 
   (* Turns a local command into an operation and immediately executes it.
      Returns the new state of the counter. *)
-  val local_update : t -> cmd -> t
+  val local_update : t -> cmd -> t * op
 
 end
 
@@ -121,6 +122,7 @@ module Counter : COUNTER = struct
 
   let get_op_vc = function (_, vc) -> vc
 
+  (* TODO: should we be incrementing the local VC for the counter here? *)
   let update cnt oper = 
     let (v, vc, ip) = cnt in
     let (cmd, vc_op) = oper in
@@ -137,16 +139,16 @@ module Counter : COUNTER = struct
                   (List.Assoc.remove vc ~equal:String.equal ip)
                   ~equal:String.equal
                   ip (ts + 1) in
-    update cnt (cmd, new_vc)
+    let new_op = (cmd, new_vc) in
+    (update cnt new_op, new_op)
 
 end
 
 (* We need multiple threads:
       1) the tcp server thread that reads messages and puts them in the in queue
-      2) a thread that processes the in queue
       3) a thread that processes user requests and puts them in the out queue
-      4) a thread that sends that out queue requests
-      5) the in queue and out queue and a mutex for each *)
+      4) a thread that sends out queue requests
+      5) the in queue and out queue and counter and a mutex *)
 
 type ctx =
   { cnt : Counter.t ref;
@@ -155,47 +157,92 @@ type ctx =
     lock : Nano_mutex.t;
   }
 
-let rec apply_ops (ctx : ctx) : never_returns =
-  Nano_mutex.lock_exn ctx.lock;
-  (* Fold over the in queue and apply all ops that are causally next
+  (* Run one round of processing in the in queue *)
+  let apply_once ctx : unit Deferred.t =
+    Nano_mutex.lock_exn ctx.lock;
+    (* Fold over the in queue and apply all ops that are causally next
      after the current vc. Prune all old ops. This fold has side effects. *)
-  let new_inq = List.fold !(ctx.inq) ~init:([] : Counter.op list)  ~f:(fun (acc_inq : Counter.op list) op ->
-    let vc = Counter.get_vc !(ctx.cnt) in
-    let op_vc = Counter.get_op_vc op in
-    match (VC.cmp vc op_vc) with
-    | VC.Gt | VC.Eq -> acc_inq (* prune old op *)
-    | _ ->
-      if (VC.causal_next ~vc_base:vc ~vc_next:op_vc) then (
-        (* We can apply the op as a side effect *)
-        ctx.cnt := Counter.update !(ctx.cnt) op;
-        (* Prune it from the list *)
-        acc_inq)
-      else
-        op :: acc_inq (* save and apply in the future*)) in
-  ctx.inq := new_inq;
-  Nano_mutex.unlock_exn ctx.lock;
-  apply_ops ctx
+    let new_inq = List.fold !(ctx.inq) ~init:([] : Counter.op list)  ~f:(fun (acc_inq : Counter.op list) op ->
+      let vc = Counter.get_vc !(ctx.cnt) in
+      let op_vc = Counter.get_op_vc op in
+      match (VC.cmp vc op_vc) with
+      | VC.Gt | VC.Eq -> acc_inq (* prune old op *)
+      | _ ->
+        if (VC.causal_next ~vc_base:vc ~vc_next:op_vc) then (
+          (* We can apply the op as a side effect *)
+          ctx.cnt := Counter.update !(ctx.cnt) op;
+          (* Prune it from the list *)
+          acc_inq)
+        else
+          op :: acc_inq (* save and apply in the future*)) in
+    ctx.inq := new_inq;
+    Nano_mutex.unlock_exn ctx.lock;
+    return ()
 
-let rec copy_blocks buffer r w =
-    Reader.read r buffer
-    >>= function
-    | `Eof -> return ()
-    | `Ok bytes_read ->
-       Writer.write w (Bytes.to_string buffer) ~len:bytes_read;
-       Writer.flushed w
-       >>= fun () ->
-       copy_blocks buffer r w
+(* Loop and apply all applicable ops in the in queue *) 
+  let rec apply_ops ctx : unit Deferred.t =
+    (* We need one bind here so that the scheduler can schedule in another "thread" *)
+    apply_once ctx >>= (fun _ -> apply_ops ctx)
 
-let run () =
-  let host_and_port =
-    Tcp.Server.create
-      ~on_handler_error:`Raise
-      (Tcp.Where_to_listen.of_port 8765)
-      (fun _addr r w ->
-        Pipe.transfer (Reader.pipe r) (Writer.pipe w) ~f:(fun x -> x))
-  in    
-  ignore (host_and_port : (Socket.Address.Inet.t, int) Tcp.Server.t Deferred.t)
+let buffer_size = 100 * 1024 (* 100 kilobytes *)
+let add_op ctx r =
+  let buffer = Bytes.create buffer_size in
+  Reader.read r buffer
+  >>= function
+  | `Eof -> return ()
+  | `Ok _ ->
+    (* TODO: this is a hack since we're only reading once. Fix. *)
+    let op = Counter.op_of_sexp (String.sexp_of_t (Bytes.to_string buffer)) in
+    Nano_mutex.lock_exn ctx.lock;
+    ctx.inq := op :: !(ctx.inq);
+    Nano_mutex.unlock_exn ctx.lock;
+    return ()
 
+let run_server ctx port =
+  Tcp.Server.create
+    ~on_handler_error:`Raise
+    (Tcp.Where_to_listen.of_port port)
+    (fun _addr r w ->
+      add_op ctx r
+      >>= (fun _ ->
+      Writer.write w "ACK";
+      Writer.flushed w))
+
+(* User-level commands *)
+type cmd_or_query =
+| Cmd of Counter.cmd
+| Query
+
+(* Read one command from stdin *)
+let get_cmd () =
+  match Caml.read_line () with
+  | "inc" -> Some (Cmd Counter.Inc)
+  | "dec" -> Some (Cmd Counter.Dec)
+  | "q" -> Some Query
+  | _ -> None
+
+let rec io_loop ctx : unit = 
+  (match get_cmd () with
+  |  None -> ()
+  | Some Query ->
+    Caml.Printf.printf "%d\n" (Counter.query !(ctx.cnt))
+  | Some (Cmd cmd) ->
+    Nano_mutex.lock_exn ctx.lock;
+    let new_cnt, _ = Counter.local_update !(ctx.cnt) cmd in
+    ctx.cnt := new_cnt;
+    Nano_mutex.unlock_exn ctx.lock);
+    Caml.Printf.printf "%d\n" (Counter.query !(ctx.cnt));
+    io_loop ctx
+  
   let () =
-    run ();
-    never_returns (Scheduler.go ())
+    (* TODO: fill in details below *)
+    let ctx = {
+      cnt = ref (Counter.init ~addrs:["192.168.0.1"] ~local_addr:"192.168.0.1");
+      inq = ref [];
+      outq = ref [];
+      lock = Nano_mutex.create ();
+    } in
+    (* let port = 0 in *)
+    (* ignore(run_server ctx port : (Socket.Address.Inet.t, int) Tcp.Server.t Deferred.t); *)
+    io_loop ctx
+    (* ignore(Thread.create ~on_uncaught_exn:`Kill_whole_process io_loop ctx : Thread.t); *)
