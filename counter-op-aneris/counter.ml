0717c8c4b9bbc9bbf22c5b72761c74d3fe5827e3
Abel Nieto <abeln@cs.au.dk>
2021-07-21 10:59:29 -0400

    counter builds

--- counter-op-aneris/counter.ml
+++ counter-op-aneris/counter.ml
@@ -14,96 +14,106 @@ let receiveFrom skt =
   let buffer = Bytes.create 1024 in
   match recvfrom skt buffer 0 1024 [] with
   | len, (ADDR_INET (addr, port) as sockaddr) ->
-    let msg = Bytes.sub_string buffer 0 len in
-    Some (msg, sockaddr)
+      let msg = Bytes.sub_string buffer 0 len in
+      Some (msg, sockaddr)
   | _ -> None
 
 let sendTo skt msg sktaddr =
-  let _ = sendto skt (Bytes.of_string msg) 0 (String.length msg) [] sktaddr
-  in ()
+  let _ = sendto skt (Bytes.of_string msg) 0 (String.length msg) [] sktaddr in
+  ()
 
 let rec listen skt handle =
-  match (receiveFrom skt) with
+  match receiveFrom skt with
   | Some msg -> handle (fst msg) (snd msg)
   | None -> listen skt handle
 
 let rec listen_wait skt =
-  match (receiveFrom skt) with
-  | Some msg -> msg
-  | None -> listen_wait skt
+  match receiveFrom skt with Some msg -> msg | None -> listen_wait skt
 
 (** Aneris.lock manual translation with Mutex *)
 let newlock () = Mutex.create ()
+
 let acquire lk = Mutex.lock lk
+
 let release lk = Mutex.unlock lk
 
 (** Aneris.dict manual translation using lists *)
 let dict_empty () = []
-let dict_remove = List.remove_assoc
-let dict_insert k v d = (k,v) :: dict_remove k d
-let dict_lookup k db =
-  try Some (List.assoc k db)
-  with Not_found -> None
 
+let dict_remove = List.remove_assoc
+
+let dict_insert k v d = (k, v) :: dict_remove k d
+
+let dict_lookup k db = try Some (List.assoc k db) with Not_found -> None
 
 (** Aneris.vector_clocks manual translation using lists *)
 let vect_make len e = List.init len (fun _ -> e)
+
 let vect_nth vc i = try List.nth vc i with Failure _ -> assert false
+
 let rec vect_update vc i v =
   match vc with
-  | w :: l -> if i = 0 then v :: l else w :: vect_update l (i-1) v
+  | w :: l -> if i = 0 then v :: l else w :: vect_update l (i - 1) v
   | [] -> []
+
 let vect_inc vc i =
   let v = vect_nth vc i + 1 in
   vect_update vc i v
-let vect_leq v1 v2 =
-  List.for_all2 (fun a b -> a <= b) v1 v2
+
+let vect_leq v1 v2 = List.for_all2 (fun a b -> a <= b) v1 v2
+
 let vect_applicable v1 v2 i =
   let rec aux j l1 l2 =
     match (l1, l2) with
     | [], [] -> true
-    | a :: q1, b :: q2 when i = j -> a = b + 1 && aux (j+1) q1 q2
-    | a :: q1, b :: q2 -> a <= b && aux (j+1) q1 q2
+    | a :: q1, b :: q2 when i = j -> a = b + 1 && aux (j + 1) q1 q2
+    | a :: q1, b :: q2 -> a <= b && aux (j + 1) q1 q2
     | _, _ -> false
-  in aux 0 v1 v2
+  in
+  aux 0 v1 v2
+
 let rec vect_serialize v =
   match v with
-  | a :: q -> String.concat  "_" ((string_of_int a) :: [vect_serialize q])
+  | a :: q -> String.concat "_" [ string_of_int a; vect_serialize q ]
   | [] -> ""
+
 let rec vect_deserialize s =
   try
     let i = String.index s '_' in
     try
       let x = int_of_string (String.sub s 0 i) in
       let vc =
-        vect_deserialize (String.sub s (i+1) (String.length s - (i + 1))) in
+        vect_deserialize (String.sub s (i + 1) (String.length s - (i + 1)))
+      in
       x :: vc
     with Failure _ -> assert false
   with Not_found -> []
 
-
 (** Aneris.serialization manual translation *)
 let string_ser x = x
+
 let string_deser x = x
+
 let int_ser n = string_of_int n
+
 let int_deser s = int_of_string s
 
 (* to simplify for time being, values are just integers *)
 let val_ser v = string_of_int v
+
 let val_deser s = int_of_string s
 
 let prod_ser serA serB v =
   let s1 = serA (fst v) in
   let s2 = serB (snd v) in
-  (string_of_int (String.length s1)) ^ "_" ^ s1 ^ s2
+  string_of_int (String.length s1) ^ "_" ^ s1 ^ s2
 
 let prod_deser deserA deserB s =
   try
     let i = String.index s '_' in
     let len = int_of_string (String.sub s 0 i) in
     let s1 = String.sub s (i + 1) len in
-    let s2 = String.sub s (i + 1 + len)
-               (String.length s - (i + 1 + len)) in
+    let s2 = String.sub s (i + 1 + len) (String.length s - (i + 1 + len)) in
     let v1 = deserA s1 in
     let v2 = deserB s2 in
     (v1, v2)
@@ -115,195 +125,187 @@ let prod_deser deserA deserB s =
    depends on the tag (the first component). *)
 
 let either_ser left_ser right_ser v =
-  match v with
-  | Left lv -> "1_" ^ (left_ser lv)
-  | Right rv -> "2_" ^ (right_ser rv)
+  match v with Left lv -> "1_" ^ left_ser lv | Right rv -> "2_" ^ right_ser rv
 
 let either_deser left_deser right_deser s =
   let i = String.index s '_' in
   let len = String.length s - 2 in
-  match (String.sub s 0 1) with
+  match String.sub s 0 1 with
   | "1" -> Left (left_deser (String.sub s (i + 1) len))
   | "2" -> Right (right_deser (String.sub s (i + 1) len))
   | _ -> assert false
 
 (* Type aliases for Ocaml but not present in Aneris *)
 type vc = int list
+
 type cmd = string * int (* ("INC", v) or ("DEC", v) *)
-type oper = ((cmd * vc) * int)
+
+type oper = (cmd * vc) * int
+
 type seqnum = int
+
 type ack = (string * seqnum) * int (* (("ACK", seqnum), sender-id) *)
 
 let mk_ack sn sender_id : ack = (("ACK", sn), sender_id)
+
 let mk_oper cmd vc sender_id : oper = ((cmd, vc), sender_id)
 
 let msg_of_oper op = Left op
+
 let msg_of_ack ack = Right ack
 
-let oper_ser = 
-  prod_ser
-    (prod_ser
-      (prod_ser string_ser int_ser)
-       vect_serialize)
-    int_ser
+let mk_inc v = ("INC", v)
+
+let mk_dec v = ("DEC", v)
+
+let oper_ser =
+  prod_ser (prod_ser (prod_ser string_ser int_ser) vect_serialize) int_ser
 
 let oper_deser =
   prod_deser
-    (prod_deser
-     (prod_deser string_deser int_deser)
-      vect_deserialize)
+    (prod_deser (prod_deser string_deser int_deser) vect_deserialize)
     int_deser
 
-let ack_ser =
-  prod_ser (prod_ser string_ser int_ser)
-           int_ser
+let ack_ser = prod_ser (prod_ser string_ser int_ser) int_ser
 
-let ack_deser =
-  prod_deser (prod_deser string_deser int_deser)
-             int_deser
+let ack_deser = prod_deser (prod_deser string_deser int_deser) int_deser
 
 let msg_ser = either_ser oper_ser ack_ser
-  
-let msg_deser = either_deser oper_deser ack_deser
-  
-(* CCDDB manual translation  *)
-let pi1 ((x,y),z) = x
-let pi2 ((x,y),z) = y
-let pi3 ((x,y),z) = z
 
-let rec find f l = match l with
+let msg_deser = either_deser oper_deser ack_deser
+
+(* CCDDB manual translation  *)
+let pi1 ((x, y), z) = x
+
+let pi2 ((x, y), z) = y
+
+let pi3 ((x, y), z) = z
+
+let rec find f l =
+  match l with
   | [] -> None
-  | hd :: tl ->
-      if f hd then Some (hd, tl) else
-        match (find f tl) with
-        | Some (hd',tl') -> Some (hd', hd :: tl')
-        | None -> None
+  | hd :: tl -> (
+      if f hd then Some (hd, tl)
+      else
+        match find f tl with
+        | Some (hd', tl') -> Some (hd', hd :: tl')
+        | None -> None)
 
 let check vc i w =
-  let (wt, wo) = (pi2 w, pi3 w) in
-  let test1 = (i != wo) in
-  let test2 = (wo < List.length vc) in
-  let test3 = (vect_applicable wt vc wo) in
+  let wt, wo = (pi2 w, pi3 w) in
+  let test1 = i != wo in
+  let test2 = wo < List.length vc in
+  let test3 = vect_applicable wt vc wo in
   test1 && test2 && test3
 
 let apply ctr vc lock (iq : oper list ref) i =
   let rec aux () =
     Thread.delay 1.;
     acquire lock;
-    begin
-      match (find (check !vc i) !iq) with
-      | Some (op, iq') ->
-         let cmd = pi1 op in
-         let sender_id = pi3 op in
-         iq := iq';
-         (match cmd with
-         | ("INC", v) -> ctr := !ctr + v
-         | ("DEC", v) -> ctr := !ctr - v
-         | _ -> assert false
-         );
-         vc := vect_inc !vc sender_id;
-      | None -> ()
-    end;
-    release lock; aux ()
-  in aux ()
+    (match find (check !vc i) !iq with
+    | Some (op, iq') ->
+        let cmd = pi1 op in
+        let sender_id = pi3 op in
+        iq := iq';
+        (match cmd with
+        | "INC", v -> ctr := !ctr + v
+        | "DEC", v -> ctr := !ctr - v
+        | _ -> assert false);
+        vc := vect_inc !vc sender_id
+    | None -> ());
+    release lock;
+    aux ()
+  in
+  aux ()
 
 let sendNext i skt mq sktaddrl acks =
   let rec aux () =
-    if (Queue.is_empty mq) then ()
+    if Queue.is_empty mq then ()
     else
-    begin
-      let (op : oper) = Queue.peek mq in 
+      let (op : oper) = Queue.peek mq in
       let vc = pi2 op in
       let dest = pi3 op in
       let sn = vect_nth vc i in
       let sn_ack = List.nth !acks dest in
-      if (sn = sn_ack) then
-        begin
-          (* The current message was acked, so we can move on
-             to the next one.  *)
-          let _ = Queue.pop mq in
-          aux ()
-        end
-      else if (sn = sn_ack + 1) then
-        begin
-          (* The current message hasn't been acked.
-             We should send or re-send it, in case
-             it wasn't previously received.  *)
-          sendTo skt (msg_ser (msg_of_oper op)) (List.nth sktaddrl dest)
-        end
+      if sn = sn_ack then
+        (* The current message was acked, so we can move on
+           to the next one. *)
+        let _ = Queue.pop mq in
+        aux ()
+      else if sn = sn_ack + 1 then
+        (* The current message hasn't been acked.
+           We should send or re-send it, in case
+           it wasn't previously received. *)
+        sendTo skt (msg_ser (msg_of_oper op)) (List.nth sktaddrl dest)
       else
-        begin
+        assert
           (* No other cases are possible:
-            - sn < sn_ack is not possible because we would've already sent the message
-            - sn > sn_ack + 1 is not possible because it means we sent two messages in a row
-              without waiting for an ack *) 
-          assert false
-        end
-    end
-    in
-    aux ()
+             - sn < sn_ack is not possible because we would've already sent the message
+             - sn > sn_ack + 1 is not possible because it means we sent two messages in a row
+               without waiting for an ack *)
+          false
+  in
+  aux ()
 
 let send_thread i skt lock l acks oq =
   let rec aux () =
     Thread.delay 1.;
     acquire lock;
     match !oq with
-    | [] -> release lock; aux ()
+    | [] ->
+        release lock;
+        aux ()
     | mq :: oq' ->
-      sendNext i skt mq l acks;
-      release lock;
-      aux ()
-  in aux ()
+        sendNext i skt mq l acks;
+        release lock;
+        aux ()
+  in
+  aux ()
 
 let receive_thread i skt lock vc iq =
   let rec aux () =
     Thread.delay 0.5;
-    let (msg, addr) = listen_wait skt in
+    let msg, addr = listen_wait skt in
     (* Printf.printf "<debug received> %s \n" msg; *)
     (* flush Stdlib.stdout; *)
     acquire lock;
-    (match (msg_deser msg) with
+    (match msg_deser msg with
     | Left op ->
         let sender_vc = pi2 op in
         let sender = pi3 op in
         let sender_sn = vect_nth sender_vc sender in
         let my_sn = vect_nth !vc sender in
-        if (sender_sn < my_sn) then
-          begin
-            (* Must be a network duplicate, so ignore it *)
-          end
-        else if (sender_sn = my_sn) then
-          begin
-            (* Could be a duplicate, but it could also be that our ack
-               got lost, so resend the ack  *)
-            sendTo skt (msg_ser (msg_of_ack (mk_ack sender_sn i))) addr
-          end
-        else if (sender_sn = my_sn + 1) then
-          begin
-            (* This is a new message from the sender, so we need to store it
-               and ack it. *)
-            iq := op :: !iq;
-            sendTo skt (msg_ser (msg_of_ack (mk_ack sender_sn i))) addr
-          end
+        if sender_sn < my_sn then
+          ( (* Must be a network duplicate, so ignore it *) )
+        else if sender_sn = my_sn then
+          (* Could be a duplicate, but it could also be that our ack
+             got lost, so resend the ack *)
+          sendTo skt (msg_ser (msg_of_ack (mk_ack sender_sn i))) addr
+        else if sender_sn = my_sn + 1 then (
+          (* This is a new message from the sender, so we need to store it
+             and ack it. *)
+          iq := op :: !iq;
+          sendTo skt (msg_ser (msg_of_ack (mk_ack sender_sn i))) addr)
         else
-          begin
+          assert
             (* We must have sender_sn > my_sn + 1, but that can't happen because
                we're running stop-and-wait *)
-            assert false
-          end
-    | Right ack -> ()
-    );
-    release lock; aux ()
-  in aux ()
+            false
+    | Right ack -> ());
+    release lock;
+    aux ()
+  in
+  aux ()
 
-let read ctr lock =
+let read ctr lock () =
   (* Printf.printf "<debug> reading db before lock \n"; *)
   acquire lock;
   Thread.delay 0.3;
   (* Printf.printf "<debug> reading db \n"; *)
   (* flush Stdlib.stdout; *)
   let r = !ctr in
-  release lock; r
+  release lock;
+  r
 
 let update ctr vc oq lock i cmd =
   acquire lock;
@@ -337,7 +339,7 @@ let init l i =
      corresponding to the operation.
   *)
   let (iq : oper list ref) = ref [] in
-  let (oq : (oper Queue.t) list ref) = ref (vect_make n (Queue.create ())) in
+  let (oq : oper Queue.t list ref) = ref (vect_make n (Queue.create ())) in
   (* A vector that maps replica indices to the highest ack
      received from that replica. *)
   let (acks : int list ref) = ref (vect_make (List.length l) 0) in
@@ -350,32 +352,35 @@ let init l i =
   (read ctr lock, update ctr vc oq lock i)
 
 (** Execution *)
-let action i rd wr =
+let action i rd upd =
   let s = read_line () in
-  match (String.split_on_char ' ' s) with
-    "read" :: k :: [] ->
-     begin
-       match rd k with
-         Some v -> Printf.printf "DB(%n)[%s] : %n\n"  i k v
-       | None -> Printf.printf "DB(%n)[%s] : None\n" i k
-     end
-  | "write" :: k :: v :: [] ->
-     let v = int_of_string v in
-     let () = wr k v in
-     Printf.printf "DB(%n)[%s <- %n]\n" i k v
+  match String.split_on_char ' ' s with
+  | [ "read" ] -> Printf.printf "CTR[%n] : %n\n" i (rd ())
+  | [ "inc"; v_str ] ->
+      let v = int_of_string v_str in
+      let () = upd (mk_inc v) in
+      Printf.printf "CTR[%n] : %n\n" i (rd ())
+  | [ "dec"; v_str ] ->
+      let v = int_of_string v_str in
+      let () = upd (mk_dec v) in
+      Printf.printf "CTR[%n] : %n\n" i (rd ())
   | "close" :: _ -> exit 0
   | _ -> Printf.printf "invalid command \n"
 
 let init_exec () =
-  if Array.length Sys.argv < 4
-  then (prerr_endline "Usage: init <index> <port1 port2 ... portN>"; exit 2);
+  if Array.length Sys.argv < 4 then (
+    prerr_endline "Usage: init <index> <port1 port2 ... portN>";
+    exit 2);
   let ip = (gethostbyname "localhost").h_addr_list.(0) in
   let l =
-    let dbsa i = makeAddress ip (int_of_string Sys.argv.(i+2)) in
-    List.init (Array.length Sys.argv - 2) dbsa in
+    let dbsa i = makeAddress ip (int_of_string Sys.argv.(i + 2)) in
+    List.init (Array.length Sys.argv - 2) dbsa
+  in
   let i = int_of_string Sys.argv.(1) in
-  let (rd, wr) = init l i in
-  while true do action i rd wr done
+  let rd, upd = init l i in
+  while true do
+    action i rd upd
+  done
 
 let () = Unix.handle_unix_error init_exec ()
 
---
+++ counter-op-aneris/format.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ocamlformat --enable-outside-detected-project --inplace counter.ml
